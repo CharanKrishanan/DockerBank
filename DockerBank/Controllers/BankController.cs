75a2f6dc7663da06b9197bcb034a07eef5009fe4
CharanKrishnan <charankovvali@gmail.com>
2023-12-13 13:46:12 +0530

    Add project files.

---
+++ DockerBank/Controllers/BankController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DockerBank.Controllers
+{
+	public class BankController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult Dashboard()
+		{
+			return View();
+		}
+
+		// Add a Logout action if needed
+		[HttpPost]
+		public IActionResult Logout()
+		{
+			// Perform logout logic here
+			return RedirectToAction("Login");
+		}
+	}
+}
---
+++ DockerBank/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DockerBank.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
